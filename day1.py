0ef1c4f7c657ed970dd0207e9588d7488fb2d83e
Joyesh Shrestha <sjoyesh2000@gmail.com>
2023-11-08 21:15:25 +0545

    day 1

---
+++ day1.py
@@ -0,0 +1,13 @@
+print("Hello, Welcome to Day 1 project")
+
+
+print("Which city you grow up in?")
+
+city = input()
+
+print("What is your pet name?")
+
+pet = input()
+
+print("BAND NAME:")
+print(city+pet)
