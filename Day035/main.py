96ac469d4fe28cc4e0c55dd1205df94e65195c56
Joyesh Shrestha <sjoyesh2000@gmail.com>
2023-12-26 12:37:58 +0545

    DAY35

---
+++ Day035/main.py
@@ -0,0 +1,42 @@
+import requests
+from twilio.rest import Client
+
+
+account_sid = "AC3aa4f88b8d618ba059571ae8cefa7e6a"
+auth_token = "15599d53c838666392b79bdaecf09c5d"
+API_KEY = "2d7b96792fae3a3fae5989bc4d585b12"
+MY_LAT = 27.717245
+MY_LONG = 85.323959
+
+parameters = {
+    "lat" : MY_LAT,
+    "lon": MY_LONG,
+    "cnt":4,
+    "appid" : API_KEY
+}
+
+# ?q=${s}&APPID=2d7b96792fae3a3fae5989bc4d585b12&units=${toggle}
+response = requests.get("http://api.openweathermap.org/data/2.5/weather",params=parameters)
+
+response.raise_for_status()
+
+weather_data = response.json()
+
+id = weather_data['weather'][0]['id']
+
+code = "6X5LQS4AS55F96E57LE6FTCQ"
+
+will_rain = False
+if id <=700:
+    will_rain = True
+
+# if will_rain:
+#     client = Client(account_sid,auth_token)
+
+#     message = client.messages \
+#             .create(
+#                 body="Its going to rain today. Rmember to bring umbrella",
+#                 from ="+15017122661",
+#                 to = "+9779843375283"
+#             )    
+
---
+++ Day035/twilliotrial.py
@@ -0,0 +1,23 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+
+verify_sid = "VAb32225cbd525cb49e64496f12b5cbec8"
+verified_number = "+9779843375283"
+
+client = Client(account_sid, auth_token)
+
+verification = client.verify.v2.services(verify_sid) \
+  .verifications \
+  .create(to=verified_number, channel="sms")
+print(verification.status)
+
+otp_code = input("Please enter the OTP:")
+
+verification_check = client.verify.v2.services(verify_sid) \
+  .verification_checks \
+  .create(to=verified_number, code=otp_code)
+print(verification_check.status)
