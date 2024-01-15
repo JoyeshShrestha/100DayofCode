ba3742bef1a61f1e35bc0eccd6903f5609995041
Joyesh Shrestha <sjoyesh2000@gmail.com>
2024-01-15 08:37:24 +0545

    C:/Program Files/Git/random and /all

---
+++ Day65_REST/main.py
@@ -0,0 +1,111 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import random
+from flask import jsonify
+'''
+Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+# Cafe TABLE Configuration
+# class Cafe(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(250), unique=True, nullable=False)
+#     map_url = db.Column(db.String(500), nullable=False)
+#     img_url = db.Column(db.String(500), nullable=False)
+#     location = db.Column(db.String(250), nullable=False)
+#     seats = db.Column(db.String(250), nullable=False)
+#     has_toilet = db.Column(db.Boolean, nullable=False)
+#     has_wifi = db.Column(db.Boolean, nullable=False)
+#     has_sockets = db.Column(db.Boolean, nullable=False)
+#     can_take_calls = db.Column(db.Boolean, nullable=False)
+#     coffee_price = db.Column(db.String(250), nullable=True)
+
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        #Method 1. 
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            #Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+        
+        #Method 2. Altenatively use Dictionary Comprehension to do the same thing.
+        # return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# HTTP GET - Read Record
+@app.route("/random")
+def get_random():
+    with app.app_context():
+        result = db.session.execute(db.select(Cafe))
+        all_cafes = result.scalars().all()
+        
+
+        random_cafe = random.choice(all_cafes)
+    #     return jsonify(Cafe={
+    #     "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+    #     "seats": random_cafe.seats,
+    #     "has_toilet": random_cafe.has_toilet,
+    #     "has_wifi": random_cafe.has_wifi,
+    #     "has_sockets": random_cafe.has_sockets,
+    #     "can_take_calls": random_cafe.can_take_calls,
+    #     "coffee_price": random_cafe.coffee_price,
+    # })
+        return jsonify(cafe=random_cafe.to_dict())
+                   
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
+@app.route('/all')
+def get_all():
+    result = db.session.execute(db.select(Cafe))
+    all_cafe = result.scalars().all()
+    all_cafe_json = [cafe.to_dict() for cafe in all_cafe]
+    return jsonify(cafe=all_cafe_json)
+if __name__ == '__main__':
+    app.run(debug=True)
