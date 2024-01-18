421b286cbf14416523f5aa1bedcb3146ccf4d53f
Joyesh Shrestha <sjoyesh2000@gmail.com>
2024-01-18 22:11:55 +0545

    Adding overall Notes to Data exploration

---
+++ Day72_DataExploration/todays_learning_pts.py
@@ -0,0 +1,17 @@
+#.head(),.tail(),.shape and .columns
+
+
+#Look for NaN values with .findna() and consider using .dropna() to clean up your DataFrame
+
+
+#Access entire columns of a DataFrame using the square bracket notation:
+#df['column name'] or df[['column name 1','column name 2','column name 3']]
+
+
+#the largest and smallest values as well as their postitions can be found with methods like .max(), .min(), .idxmax() and .idxmin()
+
+
+#can sort the DataFrame with .sort_values() and add new columns with .insert()
+
+#To create an Excel Style pivot table by grouping entries that belong to a particular category use the .groupby() method
+
